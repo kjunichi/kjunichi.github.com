1bea3dbc72f2de43f3591b40de2c063085d5586a
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2014-08-07 16:07:26 +0900

    Update where.js

--- js/where.js
+++ js/where.js
@@ -1,3 +1,14 @@
+/*
+ * ZeroClipboardのswfの読み込みの謎に迫る
+ */
+/*!
+* ZeroClipboard
+* The ZeroClipboard library provides an easy way to copy text to the clipboard using an invisible Adobe Flash movie and a JavaScript interface.
+* Copyright (c) 2014 Jon Rohan, James M. Greene
+* Licensed MIT
+* http://zeroclipboard.org/
+* v2.1.6
+*/
 // where.js
 (function() {
      var _window = window, _document = _window.document;
@@ -11,25 +22,64 @@ var _getDirPathOfUrl = function(url) {
     return dir;
 };
   
+  var _getCurrentScriptUrlFromErrorStack = function(stack) {
+    var url, matches;
+    if (typeof stack === "string" && stack) {
+      matches = stack.match(/^(?:|[^:@]*@|.+\)@(?=http[s]?|file)|.+?\s+(?: at |@)(?:[^:\(]+ )*[\(]?)((?:http[s]?|file):\/\/[\/]?.+?\/[^:\)]*?)(?::\d+)(?::\d+)?/);
+      if (matches && matches[1]) {
+        url = matches[1];
+      } else {
+        matches = stack.match(/\)@((?:http[s]?|file):\/\/[\/]?.+?\/[^:\)]*?)(?::\d+)(?::\d+)?/);
+        if (matches && matches[1]) {
+          url = matches[1];
+        }
+      }
+    }
+    return url;
+  };
+  var _getCurrentScriptUrlFromError = function() {
+    var url, err;
+    try {
+      throw new _Error();
+    } catch (e) {
+      err = e;
+    }
+    if (err) {
+      url = err.sourceURL || err.fileName || _getCurrentScriptUrlFromErrorStack(err.stack);
+    }
+    return url;
+  };
+  
  var _getCurrentScriptUrl = function() {
     var jsPath, scripts, i;
+    //HTML5だとdocument.currentScriptで取得出来て簡単らしい。
     if (_document.currentScript && (jsPath = _document.currentScript.src)) {
       return jsPath;
     }
+    
+    // 以降はcurrentScript非対応の環境向けの処理臭い。
+    
     scripts = _document.getElementsByTagName("script");
     if (scripts.length === 1) {
+         // scriptタグが一つだけなら、俺しかいないだろ。
       return scripts[0].src || undefined;
     }
+    
+    // 複数scriptタグが存在している場合の処理
     if ("readyState" in scripts[0]) {
       for (i = scripts.length; i--; ) {
         if (scripts[i].readyState === "interactive" && (jsPath = scripts[i].src)) {
+             // スクリプトタグでreadyStateがinteractiveになっているものがビンゴ！
           return jsPath;
         }
       }
     }
     if (_document.readyState === "loading" && (jsPath = scripts[scripts.length - 1].src)) {
+         // DOM構築中だったら、最後のスクリプトタグで決め打ち
       return jsPath;
     }
+    
+    // これまで手を尽くして取得できなかったら、スタックトレースより取得を試みる模様。
     if (jsPath = _getCurrentScriptUrlFromError()) {
       return jsPath;
     }

c9d32d4c2d734e842508d6a9bdb939ed1d9d95c7
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-05-18 14:40:57 +0900

    Update excel2ora.js

--- tools/excel2ora.js
+++ tools/excel2ora.js
@@ -1,24 +1,23 @@
 (function(){
-  function run() {
-     var srcText = document.getElementById("src").value;
-     var distText="";
-     //console.log(srcText);
-     var lines = srcText.split("\n");
-     if(lines[0].length === 0) {
-         return;
-     }
-     distText += "'" + lines[0]+ "\'"
-     for(var i = 1; i < lines.length; i++) {
-        distText += ",\n\'" + lines[i]+ "\'"
-     }
-     distText += "\n"
-     var elm = document.getElementById("dist");
-     elm.value=distText;
-  }
+  
   window.addEventListener("load", () => {
     const elm = document.getElementById("btn");
     elm.addEventListener("click", () => {
-      run();
+      var srcText = document.getElementById("src").value;
+      var distText="";
+       //console.log(srcText);
+       var lines = srcText.split("\n");
+       if(lines[0].length === 0) {
+           return;
+       }
+       distText += "'" + lines[0]+ "\'"
+       for(var i = 1; i < lines.length; i++) {
+          distText += ",\n\'" + lines[i]+ "\'"
+       }
+       distText += "\n"
+       var elm = document.getElementById("dist");
+       elm.value=distText;
+  }
     },false);
   },false);
 })();

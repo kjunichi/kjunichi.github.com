f64eb3dc3291e534f8b64f02b7cb0e00c25f9081
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2018-01-25 14:22:34 +0900

    Update main.js

--- tools/mdlink2html/main.js
+++ tools/mdlink2html/main.js
@@ -1,24 +1,34 @@
 window.onload = () => {
-   const convertMd2Html = (mdtext)=> {
-    const lines = mdtext.split(/\n/);
-    let buf="<ul>";
-    for(const line of lines) {
-      
-      const htmlparts = line.match(/\- \[(.*?)\]\((.*?)\)/);
-      if(htmlparts && htmlparts.length>1) {
-        buf = buf + `\n<li><a href="${htmlparts[2]}">${htmlparts[1]}</a></li>`
-      }
-      //buf = buf + "\n" + line;
-    }
-    return buf + "\n</ul>";
-  };
-  const btn = document.getElementById("executeBtn");
-  btn.addEventListener("click",()=>{
-    const inMdElm = document.getElementById("inMd");
-    const inMd = inMdElm.value;
-    const result = convertMd2Html(inMd);
-    const outHtmlElm = document.getElementById("outHtml");
-    outHtmlElm.value = result;
-    outHtmlElm.style.display = "block";
-  });
+    const escapeHtml = (text) => {
+        let out = "";
+        out = out.replace("&", "&amp;");
+        out = text.replace("<", "&lt;");
+        out = out.replace(">", "&gt;");
+        out = out.replace("\"", "&quot;");
+        out = out.replace("'", "&#039;");
+
+        return out;
+    };
+
+    const convertMd2Html = (mdtext) => {
+        const lines = mdtext.split(/\n/);
+        let buf = "<ul>";
+        for (const line of lines) {
+            const htmlparts = line.match(/\- \[(.*?)\]\((.*?)\)/);
+            if (htmlparts && htmlparts.length > 1) {
+                const link = escapeHtml(htmlparts[1]);
+                buf = buf + `\n<li><a href="${htmlparts[2]}">${link}</a></li>`
+            }
+        }
+        return buf + "\n</ul>";
+    };
+    const btn = document.getElementById("executeBtn");
+    btn.addEventListener("click", () => {
+        const inMdElm = document.getElementById("inMd");
+        const inMd = inMdElm.value;
+        const result = convertMd2Html(inMd);
+        const outHtmlElm = document.getElementById("outHtml");
+        outHtmlElm.value = result;
+        outHtmlElm.style.display = "block";
+    });
 };

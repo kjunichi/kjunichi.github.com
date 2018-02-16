b96266ead51c65a813153a7f80d5b78dabc5077c
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2018-02-16 09:38:50 +0900

    Update pages.js

--- article/js/pages.js
+++ article/js/pages.js
@@ -1,10 +1,26 @@
 const index = {
 pagetitles:[
-{"title":"Linux,
+{"title":"linux",
 "key":"6374047"},
-{"title":"MacOS",
+{"title":"macos",
 "key","6017741"}
 ]
 };
-
+const getKey = (h)=>{
+  for(const item of index.pagetitles) {
+    if(item.title === toLowerCase(h)) {
+      return item.key;
+    }
+  }
+  return null;
+};
 console.log(location.hash);
+if(location.hash) {
+  const hash = location.hash.substring(1);
+  const key = getKey(hash);
+  if(key) {
+    const elm = document.createElement("script");
+    elm.src=`https://gist.github.com/kjunichi/${key}.js`
+    document.body.appendChild(elm);
+  }
+}

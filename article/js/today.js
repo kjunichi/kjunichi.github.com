6ae0554d5bb3fae84220e4968757b47ed25e2a2d
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2018-02-20 10:55:02 +0900

    Update today.js

--- article/js/today.js
+++ article/js/today.js
@@ -1,11 +1,4 @@
-const index = {
-pagetitles:[
-{"date":"0219",
-"key":"9083526"},
-{"date":"0220",
-"key":"9104694"}
-]
-};
+let index;
 
 const getKey = (h)=>{
   console.dir(index);
@@ -24,22 +17,27 @@ const getDateStr = () => {
   return `${m}${d}`;
 };
 
-
-const dateStr = getDateStr();
-const key = getKey(dateStr);
-document._write = document.write;
-const contents=[];
-document.write = (s) => {
-   contents.push(s);
-};
-
-if(key) {
-  const elm = document.createElement("script");
-  elm.src=`https://gist.github.com/kjunichi/${key}.js`
-  elm.onload = () =>{
-    const elm2 = document.createElement("div");
-    elm2.innerHTML = contents.join("");
-    document.body.appendChild(elm2);
+fetch('/data/today.json').then((res)=>{
+  index = res.json();
+  
+  const dateStr = getDateStr();
+  const key = getKey(dateStr);
+  document._write = document.write;
+  const contents=[];
+  document.write = (s) => {
+     contents.push(s);
   };
-  document.body.appendChild(elm);
-}
+
+  if(key) {
+    const elm = document.createElement("script");
+    elm.src=`https://gist.github.com/kjunichi/${key}.js`
+    elm.onload = () =>{
+      const elm2 = document.createElement("div");
+      elm2.innerHTML = contents.join("");
+      document.body.appendChild(elm2);
+    };
+    document.body.appendChild(elm);
+  }
+
+});
+

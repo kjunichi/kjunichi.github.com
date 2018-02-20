30432a9cf05417e74b4ee3e0fa8b5a939e014691
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2018-02-20 11:27:25 +0900

    Update pages.js

--- article/js/pages.js
+++ article/js/pages.js
@@ -1,12 +1,6 @@
-const index = {
-pagetitles:[
-{"title":"linux",
-"key":"6374047"},
-{"title":"macos",
-"key":"6017741"}
-]
-};
+let index;
 
+// functions
 const getKey = (h)=>{
   console.dir(index);
   for(const item of index.pagetitles) {
@@ -17,24 +11,30 @@ const getKey = (h)=>{
   return null;
 };
 
+// main
 console.log(location.hash);
-if(location.hash) {
-  const hash = location.hash.substring(1);
-  const key = getKey(hash);
-  document._write = document.write;
-  const contents=[];
-  document.write = (s) => {
-    contents.push(s);
-  };
-  
-  if(key) {
-    const elm = document.createElement("script");
-    elm.src=`https://gist.github.com/kjunichi/${key}.js`
-    elm.onload = () =>{
-      const elm2 = document.createElement("div");
-      elm2.innerHTML = contents.join("");
-      document.body.appendChild(elm2);
+fetch('./data/page.json').then((res)=>{
+  return res.json();
+}).then((json)=>{
+  index = json;
+  if(location.hash) {
+    const hash = location.hash.substring(1);
+    const key = getKey(hash);
+    document._write = document.write;
+    const contents=[];
+    document.write = (s) => {
+      contents.push(s);
     };
-    document.body.appendChild(elm);
+
+    if(key) {
+      const elm = document.createElement("script");
+      elm.src=`https://gist.github.com/kjunichi/${key}.js`
+      elm.onload = () =>{
+        const elm2 = document.createElement("div");
+        elm2.innerHTML = contents.join("");
+        document.body.appendChild(elm2);
+      };
+      document.body.appendChild(elm);
+    }
   }
-}
+});

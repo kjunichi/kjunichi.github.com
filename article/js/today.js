0112a489db2d4b8f488906d3ea16b97c1695d282
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2018-04-25 17:34:43 +0900

    Update today.js

--- article/js/today.js
+++ article/js/today.js
@@ -1,49 +1,58 @@
 let index;
 
 // functions 
-const getKey = (h)=>{
-  console.dir(index);
-  for(const item of index.pagetitles) {
-    if(item.date === h) {
-      return item.key;
+const getKey = (h) => {
+    console.dir(index);
+    for (const item of index.pagetitles) {
+        if (item.date === h) {
+            return item.key;
+        }
     }
-  }
-  return null;
+    return null;
 };
 
 const getDateStr = () => {
-  const now = new Date(Date.now());
-  const m = ("0" + (now.getMonth() + 1)).slice(-2);
-  const d = ("0" + now.getDate()).slice(-2);
-  return `${m}${d}`;
+    const now = new Date(Date.now());
+    const m = ("0" + (now.getMonth() + 1)).slice(-2);
+    const d = ("0" + now.getDate()).slice(-2);
+    return `${m}${d}`;
 };
 
+const writeTodayArticle => (cb, elm) {
+    const dateStr = getDateStr();
+    document.title = `Today(${dateStr})`;
+
+    fetch('./data/today.json').then((res) => {
+        return res.json();
+    }).then((json) => {
+        index = json;
+
+        const key = getKey(dateStr);
+        document._write = document.write;
+        const contents = [];
+        document.write = (s) => {
+            contents.push(s);
+        };
+
+        if (key) {
+            const scriptElm = document.createElement("script");
+            scriptElm.src = `https://gist.github.com/kjunichi/${key}.js`
+            scriptElm.onload = () => {
+                let elm2;
+                if (!elm) {
+                    elm2 = document.createElement("div");
+                } else {
+                    elm2 = elm;
+                }
+                elm2.innerHTML = contents.join("");
+                if (!elm) {
+                    document.body.appendChild(elm2);
+                }
+                cb();
+            };
+            document.body.appendChild(scriptElm);
+        }
+    });
+
+};
 // main
-const dateStr = getDateStr();
-document.title = `Today(${dateStr})`;
-
-fetch('./data/today.json').then((res)=>{
-  return res.json();
-}).then((json)=>{
-  index = json;
-  
-  const key = getKey(dateStr);
-  document._write = document.write;
-  const contents=[];
-  document.write = (s) => {
-     contents.push(s);
-  };
-
-  if(key) {
-    const elm = document.createElement("script");
-    elm.src=`https://gist.github.com/kjunichi/${key}.js`
-    elm.onload = () =>{
-      const elm2 = document.createElement("div");
-      elm2.innerHTML = contents.join("");
-      document.body.appendChild(elm2);
-    };
-    document.body.appendChild(elm);
-  }
-
-});
-

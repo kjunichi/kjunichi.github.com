4692319dfccf9e2bb8d6afcc27a94614385e05db
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2021-02-09 16:44:28 +0900

    add test

--- test/menutest/main.js
+++ test/menutest/main.js
@@ -1,5 +1,5 @@
-const getContents = async (out) => {
-    fetch('https://kjunichi.github.io/link.html')
+const getContents = async (out,url) => {
+    fetch(url)
         .then(async (response) => {
             response.text().then(text => {
                 out.innerText = text
@@ -13,6 +13,23 @@ const elm = document.getElementById("lnk1")
 elm.addEventListener("click", () => {
     console.log(`test start`)
     const o = document.getElementById("out")
-    getContents(o)
+    getContents(o, "https://kjunichi.github.io/link.html")
+    console.log(`test end`)
+}, false)
+
+const elm2 = document.getElementById("lnk2")
+
+elm.addEventListener("click", () => {
+    console.log(`test start`)
+    const o = document.getElementById("out")
+    getContents(o, "")
+    console.log(`test end`)
+}, false)
+
+const elm3 = document.getElementById("lnk3")
+
+elm.addEventListener("click", () => {
+    console.log(`test start`)
+    location.href = "test.html"
     console.log(`test end`)
 }, false)
